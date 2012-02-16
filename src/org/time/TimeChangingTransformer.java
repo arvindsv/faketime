8d8f34248c4f02c51cfca2f0115115b16d97536c
Aravind SV <arvind.sv@gmail.com>
2012-02-16 23:39:31 +0530

    No luck

--- src/org/time/TimeChangingTransformer.java
+++ src/org/time/TimeChangingTransformer.java
@@ -1,10 +1,6 @@
 package org.time;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.Modifier;
-import javassist.bytecode.Descriptor;
+import javassist.*;
 import org.apache.commons.io.FileUtils;
 
 import java.io.ByteArrayInputStream;
@@ -17,31 +13,46 @@ import java.util.Random;
 public class TimeChangingTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+//        System.out.println(className);
         if (!"java/lang/System".equals(className)) {
             return null;
         }
 
+        return changeTimeMethod(classfileBuffer);
+    }
+
+    public byte[] changeTimeMethod(byte[] classfileBuffer) {
         try {
-            FileUtils.writeByteArrayToFile(new File("/tmp/realSystem" + new Random().nextInt() + ".class"), classfileBuffer);
+//            FileUtils.writeByteArrayToFile(new File("/tmp/realSystem" + new Random().nextInt() + ".class"), classfileBuffer);
 
             String timeMethodName = "currentTimeMillis";
             String renamedSystemClassName = "java.lang.System$ORIGINAL";
 
             ClassPool pool = ClassPool.getDefault();
             CtClass originalSystemClass = pool.makeClass(new ByteArrayInputStream(classfileBuffer));
+/*
             originalSystemClass.setName(renamedSystemClassName);
             originalSystemClass.setModifiers(Modifier.PUBLIC);
             originalSystemClass.getConstructor(Descriptor.ofConstructor(new CtClass[0])).setModifiers(Modifier.PROTECTED);
 
             CtClass newSystemClass = pool.makeClass(new ByteArrayInputStream(classfileBuffer));
             newSystemClass.setName("java.lang.System");
-            CtMethod newMethod = newSystemClass.getDeclaredMethod(timeMethodName);
-            newMethod.setBody("return " + renamedSystemClassName + "." + timeMethodName + "();");
+*/
+            CtMethod oldMethod = originalSystemClass.getDeclaredMethod(timeMethodName);
+            CtMethod newMethod = CtNewMethod.copy(oldMethod, originalSystemClass, null);
+            newMethod.setBody("return null;");
+            newMethod.setModifiers(Modifier.PUBLIC);
 
+            originalSystemClass.removeMethod(oldMethod);
+            originalSystemClass.addMethod(newMethod);
+
+/*
             FileUtils.writeByteArrayToFile(new File("/tmp/oldSystem" + new Random().nextInt() + ".class"), originalSystemClass.toBytecode());
-            FileUtils.writeByteArrayToFile(new File("/tmp/newSystem" + new Random().nextInt() + ".class"), newSystemClass.toBytecode());
+*/
+//            FileUtils.writeByteArrayToFile(new File("/tmp/theOneInCode.class"), IOUtils.toByteArray(getClass().getResourceAsStream("/java/lang/System.class")));
+            FileUtils.writeByteArrayToFile(new File("/tmp/newSystem" + new Random().nextInt() + ".class"), originalSystemClass.toBytecode());
 
-            return newSystemClass.toBytecode();
+            return originalSystemClass.toBytecode();
         } catch (Throwable e) {
             System.out.println(e);
             throw new RuntimeException(e);
--- src/org/time/TransformerRegistrar.java
+++ src/org/time/TransformerRegistrar.java
@@ -2,10 +2,33 @@ package org.time;
 
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.util.ArrayList;
 
 public class TransformerRegistrar {
     public static void premain(String agentArguments, Instrumentation instrumentation) throws UnmodifiableClassException {
-        instrumentation.addTransformer(new TimeChangingTransformer(), true);
-        instrumentation.retransformClasses(System.class);
+        TimeChangingTransformer transformer = new TimeChangingTransformer();
+        instrumentation.addTransformer(transformer, true);
+        instrumentation.setNativeMethodPrefix(transformer, "timeChangingAgent");
+        Class[] allLoadedClasses = instrumentation.getAllLoadedClasses();
+        ArrayList<Class> modifiableClasses = new ArrayList<Class>();
+
+        for (Class loadedClass : allLoadedClasses) {
+            if (instrumentation.isModifiableClass(loadedClass)) {
+                modifiableClasses.add(loadedClass);
+            }
+        }
+        instrumentation.retransformClasses(modifiableClasses.toArray(new Class<?>[modifiableClasses.size()]));
+/*
+        try {
+            byte[] theClassFile = new TimeChangingTransformer().changeTimeMethod(IOUtils.toByteArray(TransformerRegistrar.class.getResourceAsStream("/java/lang/System.class"))) ;
+            FileUtils.writeByteArrayToFile(new File("/tmp/gah.class"), theClassFile);
+            instrumentation.redefineClasses(new ClassDefinition(System.class, theClassFile));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+*/
+        System.out.println("Be 0: " + System.currentTimeMillis());
     }
 }
